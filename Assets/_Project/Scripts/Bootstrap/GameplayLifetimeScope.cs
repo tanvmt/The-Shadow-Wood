c3db881a635f1f0488ec2abdbaa65a025d014d55
tanvmt <163519945+tanvmt@users.noreply.github.com>
2026-09-20 00:45:40 +0700

    feat: add gameplay lifetime scope

---
+++ Assets/_Project/Scripts/Bootstrap/GameplayLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace TheShadowWood.Bootstrap
+{
+    public class GameplayLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
